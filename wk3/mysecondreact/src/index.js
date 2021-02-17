d9caa56ac2649e686ba2455c535e291da074c6e5
abbymcinerney <you@example.com>
2021-02-17 02:12:53 +0000

    activity1

--- wk3/mysecondreact/src/index.js
+++ wk3/mysecondreact/src/index.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const myfirstelement = <h2>Welcome to my first react app</h2>
-class Student extends React.Component {
-    constructor(){
-        super();
-        this.state = {webDesign: "like"};
-    }
-    render(){
-        return <h2>i am a student who {this.state.webDesign}s web design </h2>
-    }
-}
 
-// function Student (){
-//     return <h1>I am a student of type function</h1>;
+
+
+// class Student extends React.Component {
+//     constructor(){
+//         super();
+//         this.state = {webDesign: "like"};
+//     }
+//     render(){
+//         return <h2>i am a student who {this.state.webDesign}s web design </h2>
+//     }
 // }
 
+function Student (){
+    const element = (
+        <div> <h1>CSY2043 - Website Design </h1> 
+        <p>This module focuses on creating client side software as well as web applications for the World Wide Web.. </p> 
+        <ul> 
+            <li>Field: Computing</li> 
+            <li>Level: BSc</li> 
+            <li>Lecturer: Dr Muawya Eldaw</li> 
+            <li>Day: Tuesday</li> 
+            <li>Time: 16:00</li> 
+        </ul></div>
+    )
+    return element;
+        
+    
+}
 
 
-ReactDOM.render(<Student />, document.getElementById('root'));
+
+ReactDOM.render(<Student/>, document.getElementById('root'));
