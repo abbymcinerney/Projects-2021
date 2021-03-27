c9cafb6e9a86b2647418184b6db0c18fef4a92f3
abbymcinerney <you@example.com>
2021-03-27 18:07:58 +0000

    wk 6 finished

--- wk6/script.js
+++ wk6/script.js
@@ -1,19 +1,23 @@
-
-
-function myLoadFunction() {
+function updateHeader() {
     var element = document.getElementById('heading');
     element.firstChild.nodeValue = 'New Heading';
+}
+
+function updateBody() {
     var element = document.getElementById('body');
     element.firstChild.nodeValue = 'New body';
 }
 
 function myLoadFunction() {
-var element = document.getElementById('pageheading');
-element.addEventListener('click', myClickFunction);
-}
+    var element = document.getElementById('heading');
+    element.addEventListener('click', updateHeader)
 
+    var element = document.getElementById('body');
+    element.addEventListener('click', updateBody)
+    
+   }
 
-document.addEventListener('DOMContentLoaded', myLoadFunction);
+   document.addEventListener('DOMContentLoaded', myLoadFunction);
 
 function print5() {
     console.log(1);
